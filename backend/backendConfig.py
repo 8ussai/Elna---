e2a54abd09c94468708172d8bb4d748871792e2d
Qussai Abu Shehadeh <mr.qussai2004@gmail.com>
2026-06-15 15:12:44 +0300

    implement get_current_user dependency for route protection

--- backend/backendConfig.py
+++ backend/backendConfig.py
@@ -1,5 +1,10 @@
+from fastapi.security import OAuth2PasswordBearer
+
+
 SQLALCHEMY_DATABASE_URL = "sqlite:///./elna.db"
 
 SECRET_KEY = "DEVELOPMENT_TEST_KEY" 
 ALGORITHM = "HS256"               
 ACCESS_TOKEN_EXPIRE_MINUTES = 30  
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="auth/login")
---
+++ backend/core/apiDependencies.py
@@ -0,0 +1,33 @@
+from fastapi import Depends, HTTPException, status
+from sqlalchemy.orm import Session
+from jose import JWTError, jwt
+from backend.database import get_db
+from backend.models.user import User
+from backend.backendConfig import oauth2_scheme, SECRET_KEY, ALGORITHM
+
+
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)) -> User:
+
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Invalid or expired token",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+
+        username: str = payload.get("sub")
+
+        if username is None:
+            raise credentials_exception
+
+    except JWTError:
+        raise credentials_exception
+
+    user = db.query(User).filter(User.username == username).first()
+
+    if user is None:
+        raise credentials_exception
+
+    return user
